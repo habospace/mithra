a48b5f42bce7c40c8bf4755c46016c1f34296c80
habospace <avatarokwow@gmail.com>
2024-01-29 02:11:51 +0000

    feat: add runtime error module

---
+++ src/evaluator/errors.rs
@@ -0,0 +1,29 @@
+use crate::data::LineNum;
+use crate::data::MithraError;
+
+pub fn undefined_variable_err(line_num: LineNum, var_name: &String) -> MithraError {
+    MithraError::RuntimeError(format!("Variable '{}' is not defined", var_name), line_num)
+}
+
+pub fn cant_return_in_global_scope_err(line_num: LineNum) -> MithraError {
+    MithraError::RuntimeError(format!("Can't return in global scope"), line_num)
+}
+
+pub fn variable_is_not_callable_err(line_num: LineNum, var_name: &String) -> MithraError {
+    MithraError::RuntimeError(format!("'{}' is not callable", var_name), line_num)
+}
+
+pub fn incorrect_number_of_func_args_err(
+    line_num: LineNum,
+    func_name: &String,
+    n_correct_args: usize,
+    n_call_args: usize,
+) -> MithraError {
+    MithraError::RuntimeError(
+        format!(
+            "'{}' takes {} args (function called with {} args",
+            func_name, n_correct_args, n_call_args
+        ),
+        line_num,
+    )
+}
